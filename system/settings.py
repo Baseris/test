3543ae9ce641f7bbf7979be39c9b96c8856ae035
İsmail Başer <ismailbaserkayserili@gmail.com>
2018-04-13 17:40:05 +0300

    'ok'

--- system/settings.py
+++ system/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': '',
-        'HOST': '127.0.0.1',
+        'HOST': 'catalog-db',
         'PORT': '5432',
     }
 }
