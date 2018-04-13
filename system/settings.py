10da36427381ef000825440b5746a4e6a7214914
İsmail Başer <ismailbaserkayserili@gmail.com>
2018-04-13 14:26:01 +0300

    m

--- system/settings.py
+++ system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%!fcyr127f57sf6rwuzapu*p5++ee%*4x$c*p5yg7v7wcf1t_('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
